4191ea6d939f9be912df4119ae35cd3dd9e7b1e0
soham57 <sohammalviya65@gmail.com>
2023-07-03 21:03:35 +0530

    root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,22 @@
 import "@styles/globals.css";
+import { Children } from "react";
 
 export const metadata = {
   title: "Promptopia",
   description: "Discover and share creative and helpful AI Prompts",
 };
 
-const layout = () => {
-  return <div>layout</div>;
+const RootLayout = () => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
 };
 
-export default layout;
+export default RootLayout;
