9b6aa7efb169ecf954a8b0cd6afd2c84d742fbe0
soham57 <sohammalviya65@gmail.com>
2023-07-03 20:54:46 +0530

    exporting Meta data

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,10 @@
 import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover and share creative and helpful AI Prompts",
+};
+
 const layout = () => {
   return <div>layout</div>;
 };
