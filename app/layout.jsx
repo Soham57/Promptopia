3f28fbe0fbd3f5efc35d66cce6be6d54774cbaf8
soham57 <sohammalviya65@gmail.com>
2023-07-04 20:38:49 +0530

    Nav, Provider in Layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
-import { Children } from "react";
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Promptopia",
