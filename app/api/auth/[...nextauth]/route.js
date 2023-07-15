f56604c904cf1c9af8be4bdd7097c2a89c84b167
soham57 <sohammalviya65@gmail.com>
2023-07-15 12:09:38 +0530

    Using connectToDB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
   providers: [
@@ -9,6 +10,10 @@ const handler = NextAuth({
     }),
   ],
   async session({ session }) {},
-  async signIn({ profile }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+    } catch (error) {}
+  },
 });
 export { handler as GET, handler as POST };
