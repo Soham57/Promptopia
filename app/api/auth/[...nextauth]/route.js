0fad9f1e6f17acf15f31ce1192b03ac7a4225bde
soham57 <sohammalviya65@gmail.com>
2023-07-16 11:35:46 +0530

    Database connection bug fixed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,34 +10,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-      //Check if user already exists
-
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
+        //Check if user already exists
 
-      //if not create a new user
-
-      if (!userExists) {
-        await User.create({
+        const userExists = await User.findOne({
           email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
         });
-      }
 
-      return true;
-    } catch (error) {
-      return false;
-    }
+        //if not create a new user
+
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
   },
 });
 export { handler as GET, handler as POST };
