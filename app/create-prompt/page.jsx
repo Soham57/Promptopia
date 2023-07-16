c019d696095be40ba0797e8d9bd57fbbe9c0c544
soham57 <sohammalviya65@gmail.com>
2023-07-16 22:18:20 +0530

    import of createPrompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,3 +1,11 @@
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+
+import Form from "@components/Form";
+
 const CreatePrompt = () => {
   return <div>CreatePrompt</div>;
 };
