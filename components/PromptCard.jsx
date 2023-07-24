4253b6b020fd3d24a46c3525ff1637830e0edcba
soham57 <sohammalviya65@gmail.com>
2023-07-24 14:36:00 +0530

    displaying and styling post and tags

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -38,6 +38,10 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
         </div>
       </div>
+      <p className="my-4 font-satoshi tet-sm text-gray-700">{post.prompt}</p>
+      <p className="font-inter text-sm blue_gradient cursor-pointer">
+        {post.tag}
+      </p>
     </div>
   );
 };
