a991c5404b8a492faa7620de950648e085262188
soham57 <sohammalviya65@gmail.com>
2023-07-24 20:05:16 +0530

    profile dependencies

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const { data: session } = useSession();
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
