bdf835dd3cd22a50622e72c389bb3674b9f30e5a
soham57 <sohammalviya65@gmail.com>
2023-07-24 17:51:40 +0530

    route corrected

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,16 +1,14 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const POST = async (request) => {
-  const { userId, prompt, tag } = await request.json();
-
+export const GET = async (request) => {
   try {
     await connectToDB();
-    const newPrompt = new Prompt({ creator: userId, prompt, tag });
 
-    await newPrompt.save();
-    return new Response(JSON.stringify(newPrompt), { status: 200 });
+    const prompts = await Prompt.find({}).populate("creator");
+
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
-    return new Response("Failed to create a new prompt", { status: 500 });
+    return new Response("Failed to fetch all prompts", { status: 500 });
   }
 };
