140166e71467583c42ee1d223ad9da82ff7f00d0
soham57 <sohammalviya65@gmail.com>
2023-07-24 20:49:22 +0530

    Profile username

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Profile = () => {
-  return <div>Profile</div>;
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
+  return (
+    <section className="w-full">
+      <h1 className="head_text text-left">
+        <span className="blue-gradient">{name} Profile</span>
+      </h1>
+      <p>{desc}</p>
+    </section>
+  );
 };
 
 export default Profile;
