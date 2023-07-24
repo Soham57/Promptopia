ab221f100f9e8d04a285d773cdf74ea20ec63b25
soham57 <sohammalviya65@gmail.com>
2023-07-24 14:32:05 +0530

    Copy functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const [copied, setCopied] = useState("");
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -24,6 +25,17 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
               {post.creator.email}
             </p>
           </div>
+        </div>
+        <div className="copy_btn" onClick={() => {}}>
+          <Image
+            src={
+              copied === post.prompt
+                ? "/assets/icons/tick.svg"
+                : "/assets/icons/copy.svg"
+            }
+            width={12}
+            height={12}
+          />
         </div>
       </div>
     </div>
